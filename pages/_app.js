15b7abec876caa60fe76f480eeaabe0e75b69488
Jeerawat Keawsawat <gukaihero@gmail.com>
2021-05-27 17:34:33 +0700

    :art: App
    add layout

--- pages/_app.js
+++ pages/_app.js
@@ -3,17 +3,23 @@ import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/management">Management</Link>
-          </li>
-        </ul>
-      </nav>
+    <div className="mx-auto my-8 w-9/12">
+      <header>
+        <h1 className="text-6xl font-bold text-center">My Coin Kub</h1>
+        <nav className="my-4">
+          <ul className="flex flex-row justify-center space-x-4">
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/management">Management</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
       <Component {...pageProps} />
     </div>
   );
